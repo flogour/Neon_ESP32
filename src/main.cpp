2ee06eb2f08f28800e0e2cfa123f463cebf03498
flogour <florent.gourmelen@gmail.com>
2023-07-05 18:58:31 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -46,6 +46,10 @@ const char* index_html = R"html(
         <option value="0">Aucune</option>
         <option value="1">Animation 1</option>
         <option value="2">Animation 2</option>
+        <option value="3">Animation 3</option>
+        <option value="4">Animation 4</option>
+        <option value="5">Animation 5</option>
+        <option value="6">Animation 6</option>
     </select>
 
     <script>
@@ -71,10 +75,70 @@ const char* index_html = R"html(
 )html";
 
 
+
+
+
+void setup() {
+  // Connexion au réseau Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connexion au WiFi...");
+  }
+
+  // Configuration du ruban LED
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.show();
+
+  // Routes du serveur web
+  server.on("/", handleRoot);
+  server.on("/color", handleColor);
+  server.on("/animation", handleAnimation);
+
+  // Démarrage du serveur web
+  server.begin();
+
+  Serial.println("Serveur démarré !");
+}
+
+
+void loop() {
+  // Gestion des requêtes du serveur web
+  server.handleClient();
+
+  // Ajoutez ici le code pour les animations du ruban LED
+}
+
+
+
+
+
+
+void handleRoot() {
+  String htmlContent = String(index_html);
+  server.send(200, "text/html", htmlContent);
+}
+
+// Route pour définir la couleur des LEDs
+void handleColor() {
+  r = server.arg("r").toInt();
+  g = server.arg("g").toInt();
+  b = server.arg("b").toInt();
+  setLedColor(r, g, b);
+  server.send(200, "text/plain", "Couleur définie !");
+}
+
 // Fonction pour définir la couleur des LEDs
 void setLedColor(int r, int g, int b) {
   fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
   FastLED.show();
+}
+
+// Route pour définir l'animation des LEDs
+void handleAnimation() {
+  int mode = server.arg("mode").toInt();
+  setAnimation(mode);
+  server.send(200, "text/plain", "Animation définie !");
 }
 
 // Fonction pour définir l'animation des LEDs
@@ -100,6 +164,12 @@ void setAnimation(int mode) {
       }
     }
     fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
+    FastLED.show();
+    break;
+  
+  case 2:
+    pride();
+    FastLED.show();
     break;
   
   default:
@@ -108,53 +178,44 @@ void setAnimation(int mode) {
 }
 
 
-void handleRoot() {
-  String htmlContent = String(index_html);
-  server.send(200, "text/html", htmlContent);
-}
 
-// Route pour définir la couleur des LEDs
-void handleColor() {
-  r = server.arg("r").toInt();
-  g = server.arg("g").toInt();
-  b = server.arg("b").toInt();
-  setLedColor(r, g, b);
-  server.send(200, "text/plain", "Couleur définie !");
-}
+void pride() 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
 
-// Route pour définir l'animation des LEDs
-void handleAnimation() {
-  int mode = server.arg("mode").toInt();
-  setAnimation(mode);
-  server.send(200, "text/plain", "Animation définie !");
-}
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
 
-void setup() {
-  // Connexion au réseau Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connexion au WiFi...");
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+    
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS-1) - pixelnumber;
+    
+    nblend( leds[pixelnumber], newcolor, 64);
   }
-
-  // Configuration du ruban LED
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.show();
-
-  // Routes du serveur web
-  server.on("/", handleRoot);
-  server.on("/color", handleColor);
-  server.on("/animation", handleAnimation);
-
-  // Démarrage du serveur web
-  server.begin();
-
-  Serial.println("Serveur démarré !");
-}
-
-void loop() {
-  // Gestion des requêtes du serveur web
-  server.handleClient();
-
-  // Ajoutez ici le code pour les animations du ruban LED
 }
